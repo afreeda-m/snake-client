1226ab980bc2b1e880cd2630dd7028dabc64abf1
Afreeda <amahesan@uoguelph.ca>
2023-06-13 16:10:36 -0400

    try setInterval and play with mooves

--- client.js
+++ client.js
@@ -13,6 +13,9 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: AAM");
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 1000)
   });
 
   conn.on("data", (data) => {
