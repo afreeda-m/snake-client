f08383f3e2fcc158b52ad7a5dfdc1e8506947baf
Afreeda <amahesan@uoguelph.ca>
2023-06-14 09:30:07 -0400

    Add stdin object in function to take user input

--- play.js
+++ play.js
@@ -1,3 +1,34 @@
 const connect = require('./client');
 
-connect();
+const conn = connect();
+
+const handleUserInput = function(data) {
+  if (data === '\u0003'){
+    process.exit();
+  } else if (data === 'w') {
+    conn.write("Move: up");
+  } else if (data === 'a') {
+    conn.write("Move: left");
+  } else if (data === 's') {
+    conn.write("Move: down");
+  } else if (data === 'd') {
+    conn.write("Move: right");
+  }
+};
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput)
+
+  return stdin;
+};
+
+setupInput();
+
+module.exports = setupInput;
