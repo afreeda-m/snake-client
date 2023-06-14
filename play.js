77222d3e654e31fb116675c5adc1207064f5194d
Afreeda <amahesan@uoguelph.ca>
2023-06-14 14:41:35 -0400

    move code for handling user input to input.js

--- play.js
+++ play.js
@@ -1,34 +1,7 @@
-const connect = require('./client');
+const { connect } = require('./client');
+const { setupInput } = require('./input');
 
+console.log("Connecting....")
 const conn = connect();
 
-const handleUserInput = function(data) {
-  if (data === '\u0003'){
-    process.exit();
-  } else if (data === 'w') {
-    conn.write("Move: up");
-  } else if (data === 'a') {
-    conn.write("Move: left");
-  } else if (data === 's') {
-    conn.write("Move: down");
-  } else if (data === 'd') {
-    conn.write("Move: right");
-  }
-};
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput)
-
-  return stdin;
-};
-
-setupInput();
-
-module.exports = setupInput;
+setupInput(conn);
