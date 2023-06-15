a200a43f7547c66dee8718764409e45678cbdd3f
Afreeda <amahesan@uoguelph.ca>
2023-06-15 15:24:14 -0400

    add comments for clarity and eslint code

--- play.js
+++ play.js
@@ -1,7 +1,10 @@
 const { connect } = require('./client');
 const { setupInput } = require('./input');
 
-console.log("Connecting....")
+console.log("Connecting....");
+
+// activate connection
 const conn = connect();
 
+// allow connection to take user input
 setupInput(conn);
