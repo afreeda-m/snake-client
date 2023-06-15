1d4d6fddb6a1689258c64c0a48c6f96b03064acd
Afreeda <amahesan@uoguelph.ca>
2023-06-15 15:26:21 -0400

    minor comment edits and eslint code for format

--- input.js
+++ input.js
@@ -3,24 +3,25 @@ const { moveKeys } = require("./constants");
 //Store the active TCP connection object
 let connection;
 
+// check for user key input to send server commands
 const handleUserInput = function(data) {
-  if (data === '\u0003'){
+  if (data === '\u0003') {
     process.exit();
-  } else if (Object.keys(moveKeys).includes(data)){
+  } else if (Object.keys(moveKeys).includes(data)) {
     connection.write(moveKeys[data]);
   }
 };
 
 // setup interface to handle user input from stdin
 
-const setupInput = function (conn) {
+const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
-  stdin.on("data", handleUserInput)
+  stdin.on("data", handleUserInput);
 
   return stdin;
 };
