43ea9f19083d18fde5a36651afe5b1790ba09f48
Afreeda <amahesan@uoguelph.ca>
2023-06-15 15:04:29 -0400

    add all constants to this file

--- constants.js
+++ constants.js
@@ -2,7 +2,21 @@ const IP = 'localhost';
 
 const PORT = 50541;
 
+const playerName = "Name: AAM";
+
+const moveKeys = {
+  'w': 'Move: up',
+  'a': 'Move: left',
+  's': 'Move: down',
+  'd': 'Move: right',
+  '1': "Say: Let's play",
+  '2': "Say: Funnnnnnn!",
+  '3': "Say: ssss.....yumm!"
+}
+
 module.exports = {
   IP, 
   PORT,
+  playerName,
+  moveKeys
 }
